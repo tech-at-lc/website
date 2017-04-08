d42216a33750ca5f6230724c61b12c2f7e9e3782
Fatama Rahman <FatamaRahman@gmail.com>
2017-04-07 23:15:21 -0400

    original files

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+/* ============================================================
+ * Plugin Core Init
+ * For DEMO purposes only. Extract what you need.
+ * ============================================================ */
+ 
+'use strict';
+
+$(document).ready(function() {
+
+	/*
+		Write your custom JS here
+	*/
+    
+
+});
